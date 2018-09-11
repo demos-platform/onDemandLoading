26b4363e06450878584c66da17a413605c0834c7
Muyunyun <328375795@qq.com>
2018-09-11 10:48:01 +0800

    test: test babel-plugin-on-demand-loading 0.0.8

--- src/index.js
+++ src/index.js
@@ -7,5 +7,19 @@
 // console.log(equal)   // 未压缩
 
 // 测试三
-import { equal } from 'diana'
-console.log(equal)
+// import { equal } from 'diana'
+// console.log(equal)   // 压缩
+
+// 测试四：测试 isType 类型
+// import { isFunction } from 'diana'
+// console.log('isFunction:', isFunction)
+
+// 测试五：同时引人多个
+// import { isFunction, equal, isString } from 'diana'
+// console.log('isFunction:', isFunction)
+// console.log('equal:', equal)
+// console.log('isString:', isString)
+
+// 测试六：测试 math
+import { distance } from 'diana'
+console.log('distance', distance)
